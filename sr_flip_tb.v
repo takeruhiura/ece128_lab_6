0540b73292b61d26630e346496a103953e211172
takeruhiura <157721288+takeruhiura@users.noreply.github.com>
2025-10-22 20:39:07 -0400

    Update sr_flip_tb.v

--- sr_flip_tb.v
+++ sr_flip_tb.v
@@ -20,11 +20,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module sr_tb;
-reg S,R, CLK;
-wire Q, QBAR;
-srff dut(.q(Q), .qbar(QBAR), .s(S), .r(R), .clk(CLK)); // instantiation by
-port name.
+module sr_flip_tb;
+reg S,R, clk;
+wire Q, Qbar;
+sr_flip dut(.Q(Q), .Qbar(Qbar), .S(S), .R(R), .clk(clk)); // instantiation by port name.
 // apply test vectors
 initial begin
  clk=0;
@@ -35,5 +34,7 @@ S= 1; R= 0;
 #100; S= 0; R= 1;
 #100; S= 0; R= 0;
 #100; S= 1; R=1;
+#100;
+$finish;
 end
 endmodule
